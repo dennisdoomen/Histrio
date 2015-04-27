a8dd6ab72fdc7d8dbd0b9e1867604bc29165c1ad
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-04-27 13:09:14 +0200

    edited comments

--- src/Histrio/Observer.cs
+++ src/Histrio/Observer.cs
@@ -7,16 +7,17 @@ namespace Histrio
     public abstract class Observer
     {
         /// <summary>
-        ///     Accepts the specified message and checkes if the message can be handled by this behavior.
-        ///     If so, it notifies the message it can be handled. It uses a double dispatch to let the message
-        ///     handle itself in s strongly typed fashion by the behavior
+        ///     Accepts the specified observable and checks if the observable can be handled by the observer.
+        ///     If so, it notifies the observable it can be handled. It uses a double dispatch to let the observable
+        ///     handle itself in a strongly typed fashion by an <see cref="IObserver{T}" /> that the concrete Obserer
+        ///     also may implement
         /// </summary>
         /// ///
-        /// <typeparam name="T">The type of content the message enbodies</typeparam>
-        /// <param name="observable">The message.</param>
+        /// <typeparam name="T">The type of the subject the observable represents</typeparam>
+        /// <param name="observable">The observable.</param>
         /// <exception cref="InvalidOperationException">
-        ///     The exception is thrown when the behavior can not handle this type of
-        ///     message content
+        ///     The exception is thrown when no <see cref="IObserver{T}" />, implemented by the concrete Observer,
+        ///     can handle the subject of the observable
         /// </exception>
         public virtual void OnNext<T>(Observable<T> observable)
         {
